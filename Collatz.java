615b65f02ae80c576b2e7a2c52ceda4c0d08601d
Eden <ghirme@uw.edu>
2013-12-05 10:15:04 -0800

    Initial Commit

---
+++ Collatz.java
@@ -0,0 +1,33 @@
+/*
+Collatz Conjecture - Start with a number n > 1.
+Find the number of steps it takes to reach one using the following process: 
+If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1
+*/
+
+import java.util.*;
+
+public class Collatz {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		System.out.println("Collatz Conjecture:");
+		System.out.print("Enter a number: ");
+		int num = console.nextInt();
+		System.out.println("It takes " + solve(num) + " steps to reach 1 using the Collatz Conjecture,"); 
+		System.out.println("beginning with the number " + num + ".");
+		
+	}
+	
+	// collatz algorithm 
+	// pre-condition: n > 1
+	public static int solve(int n) {
+		if (n == 1) {
+			return 0; 
+		} else if (n % 2 == 0) {
+			return 1 + solve(n / 2);
+		} else {
+			return 1 + solve(n * 3 + 1);
+		}	 	
+	}
+
+}
---
+++ factorial.java
@@ -0,0 +1,48 @@
+/*
+Factorial Finder - The Factorial of a positive integer, n,
+is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0,
+is defined as being 1. Solve this using both loops and recursion. */
+
+import java.util.*;
+
+public class factorial {
+	public static void main(String[] args) {
+	
+	System.out.println("Find with recursion:");
+	System.out.println("1 factorial is: " + findRecurse(1));
+	System.out.println("5 factorial is: " + findRecurse(5));	
+	System.out.println("10 factorial is: " + findRecurse(10));
+	System.out.println();
+	
+	System.out.println("Find with loops:");
+	System.out.println("1 factorial is: " + findLoop(1));
+	System.out.println("5 factorial is: " + findLoop(5));	
+	System.out.println("10 factorial is: " + findLoop(10));
+	
+	
+	}
+	
+	// finds factorial with recursion
+	// pre-condition: n >= 0
+	public static int findRecurse(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return n * findRecurse(n - 1);	
+		} 
+	}
+	
+	// finds factorial with loops
+	// pre-condition: n >= 0
+	public static int findLoop(int n) {
+		int product = 1;
+		
+		while (n != 0) {
+			product *= n;
+			n--;
+		}
+		
+		return product;
+	}
+	
+}
