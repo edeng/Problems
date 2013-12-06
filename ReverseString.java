506b7e69cc274daff83f67922503bc4cbccf9e14
Eden Ghirmai <edeng94@gmail.com>
2013-12-06 11:28:20 -0800

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,15 +1,23 @@
-/*Reverse a String - Enter a string and the program will reverse it and print it out.*/
+/*
+* Eden Ghirmai
+* ReverseString.java
+*
+* Reverse a String - Enter a string and the program will reverse it and print it out.*/
 
 public class ReverseString {
 	public static void main(String[] args) {
-	Scanner console = new Scanner(System.in);
-	
-	System.out.print("What word/phrase would you like to reverse? ");
-	String word  = console.next();
-	
-	}
-	
-	public static String reverse(String n) {
-		int size = 
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("What phrase would you like to reverse?: ");
+		String phrase = in.nextLine();
+		
+		String result = "";
+		
+		for (int i = phrase.length() - 1; i >= 0; i--) {
+			String letter = phrase.substring(i, i + 1);
+			result += letter;
+		}
+		
+		System.out.println("Reversed Phrase: " + result);
 	}
 }
