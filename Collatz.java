b5b951808fd77884ff48250e4ed990e1f1c6b8de
Eden Ghirmai <edeng94@gmail.com>
2013-12-05 17:19:23 -0800

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,7 +1,10 @@
 /*
-Collatz Conjecture - Start with a number n > 1.
-Find the number of steps it takes to reach one using the following process: 
-If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1
+* Eden Ghirmai
+* Collatz.java
+*
+* Collatz Conjecture - Start with a number n > 1.
+* Find the number of steps it takes to reach one using the following process: 
+* If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1
 */
 
 import java.util.*;
