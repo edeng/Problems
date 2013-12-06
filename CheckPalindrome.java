d1f788a26beaefacefad8b2eb7943c3a5aecacfe
Eden Ghirmai <edeng94@gmail.com>
2013-12-06 11:39:50 -0800

    Update CheckPalindrome.java

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -3,7 +3,7 @@
  * CheckPalindrome.java
  * 
  * Checks if the string entered by the user is a palindrome. 
- * That is that it reads the same forwards as backwards like “racecar” */
+ * That is that it reads the same forwards as backwards like "racecar" */
 
 import java.util.*;
 
