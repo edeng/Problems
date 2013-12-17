efce833e72399ad260ab83a40e3182d8d948c77b
Eden Ghirmai <edeng94@gmail.com>
2013-12-17 12:13:54 -0800

    Update PigLatin.java

--- PigLatin.java
+++ PigLatin.java
@@ -4,7 +4,8 @@
  * 
  * Pig Latin is a game of alterations played on the English language game.
  * To create the Pig Latin form of an English word the initial consonant sound is 
- * transposed to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay).*/
+ * transposed to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay).
+ */
 
 import java.util.*;
 
