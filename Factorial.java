8faa5335ae08310e06bc44212b971cf7fb6a2e71
Eden Ghirmai <edeng94@gmail.com>
2013-12-18 23:23:39 -0800

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,7 +4,8 @@
 *
 * Factorial Finder - The Factorial of a positive integer, n,
 * is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0,
-* is defined as being 1. Solve this using both loops and recursion. */
+* is defined as being 1. Solve this using both loops and recursion. 
+*/
 
 import java.util.*;
 
